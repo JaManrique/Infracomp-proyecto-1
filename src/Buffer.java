664e15c4fde5ad81ca4a57a0d10e4579a6377546
js.diaz <js.diaz@DTI-EMLP5B008.ad.uniandes.edu.co>
2018-02-26 19:25:07 -0500

    Responder y properties

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Queue;
-
 public class Buffer {
 
 	private int capacidad;
@@ -20,12 +23,31 @@ public class Buffer {
 		}
 	}
 	
-	public synchronized Mensaje responder()
+	public synchronized void responder()
 	{
-		
+		while(buff.isEmpty()){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		Mensaje msg = buff.remove();
+		msg.setRespuesta(Integer.toString(Integer.parseInt(msg.getConsulta()) + 1));
+		msg.notify();
 	}
 	
 	public static void main(String[] args) {
+		Properties p = new Properties();
+		try {
+			p.load(new FileReader(new File("./data/params.properties")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int numClientes = Integer.parseInt(p.getProperty("numClientes"));
+		int numServidores = Integer.parseInt(p.getProperty("numServidores"));
+		int numConsultas = Integer.parseInt(p.getProperty("numConsultas"));
+		
 		
 	}
 }
