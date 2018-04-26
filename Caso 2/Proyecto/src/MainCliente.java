ca3426b6ffed7ab86614989f5f4e4f26a7c3c805
Juan Sebastian Diaz <js.diaz@uniandes.edu.co>
2018-04-26 16:32:58 -0500

    finito archivo

--- Caso 2/Proyecto/src/MainCliente.java
+++ Caso 2/Proyecto/src/MainCliente.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,11 +12,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 
 
 
-public class MainCliente {
+public class MainCliente extends Thread{
 	private static final String HOLA = "HOLA";
 	private static final String INICIO = "INICIO";
 	private static final String ALG = "ALGORITMOS";
@@ -32,104 +32,96 @@ public class MainCliente {
 	private static final String MD5 = "HMACMD5";
 	private static final String SHA1 = "HMACSHA1";
 	private static final String SHA256 = "HMACSHA256";
-	
+
 	private static ManejadorX509 X509;
 
 	private String serverIp = "localhost";
 	private Key llaveCliente = null;
 	private long t1, t2;
-	private static int nErrores = 0;
-	private static List<Long> listT1 = new ArrayList<>();
-	private static List<Long> listT2 = new ArrayList<>();
+	private boolean error;
 	private static String log = "./data/log.txt";
-	
+	private static String params = "./data/params.txt";
+
 	public MainCliente() {
 		X509 = new ManejadorX509();
+		t1 = 0;
+		t2 = 0;
+		error = false;
 	}
-	
+
 	public void reportarEstado() throws IOException, Exception{
 		Socket socket = new Socket(serverIp, 8080);
 		OutputStream os = socket.getOutputStream();
 		InputStream is = socket.getInputStream();
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		PrintWriter pw = new PrintWriter(os, true);
-		PrintWriter logger = new PrintWriter(new File(log));
-		logger.println("Inicio");
-		
+
 		pw.println(HOLA);
 		String s = br.readLine();
 		if(!INICIO.equals(s)){
-			nErrores++;
-			logger.println("Error");
+			error = true;
 			cerrarRecursos(socket, br, pw);
 			throw new Exception("error al iniciar");
 		}
 		pw.println(ALG + ":" + AES + ":" + RSA + ":" + MD5);
-		
-		verificarEstado(br.readLine(), logger);
-		
+
+		verificarEstado(br.readLine());
+
 		pw.println(CERT_CLIENTE);
 		os.write(X509.darCertCliente());
 		os.flush();
-		
-		verificarEstado(br.readLine(), logger);
-		
+
+		verificarEstado(br.readLine());
+
 		s = br.readLine();
 		if(!CERT_SERVIDOR.equals(s)){
-			nErrores++;
-			logger.println("Error");
+			error = true;
 			cerrarRecursos(socket, br, pw);
 			throw new Exception("error certsrv");
 		}
-		
+
 		byte[] certsrv = new byte[0];
-		
+
 		//Wait for bytes to be written on the socket
 		while (certsrv.length == 0) {
 			//System.out.println("certsv len: " + certsrv.length + " // actual len: " + is.available());
 			certsrv = new byte[is.available()];
 		}
-		
+
 		is.read(certsrv);
-		long t1 = System.currentTimeMillis();
-		
+		long t = System.currentTimeMillis();
+
 		if(X509.verificarCertServidor(certsrv)){
 			pw.println(ESTADO_OK);
 		}
 		else{
 			pw.println(ESTADO_ERROR);
-			nErrores++;
-			logger.println("Error");
+			error = true;
 			cerrarRecursos(socket, br, pw);
 			throw new Exception("certificado erroneo");
 		}
 
 		Key llaveServ = X509.extraerLlave(certsrv);
-		t1 = System.currentTimeMillis() - t1;
-		listT1.add(t1);
-		logger.println("t1 " + t1);
-		
+		t1 = System.currentTimeMillis() - t;
+
 		String[] S = br.readLine().split(":");
-		
+
 		if(!S[0].equals(INICIO)){
-			nErrores++;
-			logger.println("Error");
+			error = true;
 			cerrarRecursos(socket, br, pw);
 			throw new Exception("error al iniciar");
 		}
 		byte[] bytesSesion = ManejadorRSA.descifrar(X509.darLlavePrivada(), S[1]);
 		Key llaveSesion = new SecretKeySpec(bytesSesion, 0, bytesSesion.length, "AES");
-		
+
 		String pos = "41 24.2028, 2 10.4418";
-		long t2 = System.currentTimeMillis();
+		t = System.currentTimeMillis();
 		pw.println(ACT1 + ":" + ManejadorAES.cifrar(llaveSesion, pos));
 		pw.println(ACT2 + ":" + ManejadorRSA.cifrar(llaveServ, ManejadorMD5.hash(pos)));
-		
-		verificarEstado(br.readLine(), logger);
-		t2 = System.currentTimeMillis() - t2;
-		listT2.add(t2);
-		logger.println("t2 " + t2);
-		
+
+		verificarEstado(br.readLine());
+		t2 = System.currentTimeMillis() - t;
+
 		cerrarRecursos(socket, br, pw);
 
 	}
@@ -140,25 +132,76 @@ public class MainCliente {
 		socket.close();
 	}
 
-	private void verificarEstado(String S, PrintWriter logger) throws Exception{
+	private void verificarEstado(String S) throws Exception{
 		if(ESTADO_OK.equals(S)){
 			return;
 		}
 		else if(ESTADO_ERROR.equals(S)){
-			nErrores++;
-			logger.println("Error");
+			error = true;
 			throw new Exception("estado error");
 		}
 		else{
-			nErrores++;
-			logger.println("Error");
+			error = true;
 			throw new Exception("error comunicacion");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		try {
-			new MainCliente().reportarEstado();
+			int nIteraciones = 0, rampUp = 0;
+			BufferedReader br = new BufferedReader(new FileReader(params));
+			nIteraciones = Integer.parseInt(br.readLine());
+			rampUp = Integer.parseInt(br.readLine());
+			
+			long start = System.currentTimeMillis();
+			List<MainCliente> threadList = new ArrayList<>();
+			for(int i=0; i < nIteraciones; i++) {
+				MainCliente cl = new MainCliente();
+				threadList.add(cl);
+				cl.start();
+				sleep(rampUp);
+			}
+
+			for(MainCliente thread : threadList) {
+				thread.join();
+			}
+			long end = System.currentTimeMillis();
+			
+			long keyCreationTime = 0, updateTime = 0;
+			int numKeyTimes = 0, numUpdateTimes = 0, failedRequests = 0;
+			for(MainCliente cliente : threadList) {
+				if(cliente.t1 > 0) {
+					keyCreationTime += cliente.t1;
+					numKeyTimes++;
+				}
+				if(cliente.t2 > 0) {
+					updateTime += cliente.t2;
+					numUpdateTimes++;
+				}
+				if(cliente.error) {
+					failedRequests++;
+				}
+			}
+			keyCreationTime = keyCreationTime/numKeyTimes;
+			updateTime = updateTime/numUpdateTimes;
+			String type = nIteraciones + " iteraciones, ramp up de " + rampUp + " ms, con Seguridad";
+
+			PrintWriter logger = new PrintWriter(new File(log));
+			logger.print(start + "," + end + "," + type + "," + keyCreationTime + "," + updateTime + "," + failedRequests);
+			br.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		try {
+			reportarEstado();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
