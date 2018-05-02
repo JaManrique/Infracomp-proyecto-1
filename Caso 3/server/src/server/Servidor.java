e63378eabe88471cfd0ae40ee40481afee5e204c
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-05-01 21:40:58 -0500

    csv

--- Caso 3/server/src/server/Servidor.java
+++ Caso 3/server/src/server/Servidor.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors;
 
 public class Servidor {
 	private static final int TIME_OUT = 5000;
-	public static final int N_THREADS = 8;
+	public static final int N_THREADS = 1;
 	private static ServerSocket elSocket;
 	private static Servidor elServidor;
 
